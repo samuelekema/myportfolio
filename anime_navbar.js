5519205c7bf745c63f81f359b6bc0009a793c587
Samuel <ekemasamuel954@gmail.com>
2024-02-08 11:46:48 -0500

    initial commit

--- anime_navbar.js
+++ anime_navbar.js
@@ -1,18 +1,6 @@
-const searchicon = document.querySelector(".fa-search");
+const searchIcon = document.querySelector(".fa-search");
+searchIcon.addEventListener("click", function(){
+    const inputEl = document.querySelector(".input-opt");
+    inputEl.classList.toggle("toggle");
 
-searchicon.addEventListener("click", ()=>{
-    const searchBar = document.getElementById("search-bar");
-    const loginLink = document.querySelector(".login-link");
-    const submitSearch = document.getElementById("submit-search");
-    if(searchBar.style.display == "none") {
-        searchBar.style.display = "inline-block";
-        searchicon.style.display = "none"
-        loginLink.style.display = "none";
-        submitSearch.style.display = "inline-block";
-    } else {
-        searchBar.style.display = "none";
-        searchicon.style.display = "inline-block";
-        loginLink.style.display = "inline-block";
-        submitSearch.style.display = 'none';
-    }
 })
