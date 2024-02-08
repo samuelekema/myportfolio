7944487d932a52336c0b78fabdb3ca24819450ef
samuelekema <100537240+samuelekema@users.noreply.github.com>
2024-02-07 19:46:15 -0500

    Update weather_front.js

--- weather_front.js
+++ weather_front.js
@@ -3,14 +3,11 @@ goBtn.addEventListener("click", function(){
     const container = document.getElementById("container");
     container.classList.add("off-screen");
     const weatherApp = document.getElementById("weather-app");
+    weatherApp.style.display = "inline-block"
     weatherApp.style.overflow = "scroll";
     weatherApp.classList.remove("off-screen");
     const input = document.getElementById("location-input").value.reload;
 
 })
-
-
-
-//***back btn */
 
 
