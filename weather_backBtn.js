da9b2269e19b789d1c03a6b92339b98c972229a1
Samuel <ekemasamuel954@gmail.com>
2024-02-07 20:16:22 -0500

    initial commit

--- weather_backBtn.js
+++
@@ -1,8 +0,0 @@
-const backBtn = document.getElementById("back-btn");
-backBtn.addEventListener("click", function(){
-    const container = document.getElementById("container");
-    const weatherApp = document.getElementById("weather-app");
-    weatherApp.classList.add("off-screen");
-    container.classList.remove("off-screen");
-
-})
--- weather_front.js
+++ weather_front.js
@@ -1,14 +1,3 @@
-const goBtn = document.getElementById("go-btn");
-goBtn.addEventListener("click", function(){
-    const container = document.getElementById("container");
-    container.classList.add("off-screen");
-    const weatherApp = document.getElementById("weather-app");
-    weatherApp.style.overflow = "scroll";
-    weatherApp.classList.remove("off-screen");
-    const input = document.getElementById("location-input").value.reload;
-
-})
-
 
 
 //***back btn */
